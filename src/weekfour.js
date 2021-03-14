f13812047fa685f31d554f940a80062d76e79217
Lewis <Lewis-saunders@live.nmit.ac.nz>
2021-03-15 12:48:53 +1300

    did js stuff

---
+++ src/weekfour.js
@@ -0,0 +1,18 @@
+let country = "New Zealand"
+let city = "Nelson"
+let postalCode = 7010
+
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof postalCode);
+
+// declared Variable, named variable, assigned string value to city
+//*line 6 - Let is the declared variable - City is the Variable name - Nelson is the Assigned string value
+
+let isIsland = true
+let language
+//if something hasnt been given a value - look for a comment - or if you plan on doing it - make sure you leave a comment
+
+console.log(typeof isIsland)
+console.log(typeof language)
+
