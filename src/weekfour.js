5597fdc388a47fe777e35bf77cfce4df90cf0271
Lewis <Lewis-saunders@live.nmit.ac.nz>
2021-03-16 09:07:42 +1300

    Learning stuff

--- src/weekfour.js
+++ src/weekfour.js
@@ -7,7 +7,7 @@ console.log(typeof city);
 console.log(typeof postalCode);
 
 // declared Variable, named variable, assigned string value to city
-//*line 6 - Let is the declared variable - City is the Variable name - Nelson is the Assigned string value
+//*line 2 - Let is the declared variable - City is the Variable name - Nelson is the Assigned string value
 
 let isIsland = true
 let language
@@ -15,4 +15,32 @@ let language
 
 console.log(typeof isIsland)
 console.log(typeof language)
+
+language = "arabic"
+console.log(language)
+
+let number = 10
+console.log(number)
+
+console.log(number /2)
+console.log(number >6)
+console.log(number <2)
+
+const address = country + " " + city + " " + postalCode
+console.log(address)
+
+let string = `My address is ${country}, ${city}, and ${postalCode}`
+
+console.log(string)
+
+let x ='Ali';
+
+if(x === 'Ali' && typeof 'string') {
+    console.log('its true')
+ } 
+ else {
+    console.log('its false')
+}
+
+//Double bars "||" = or and double & symbol "&&" = and
 
