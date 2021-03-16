2cab22561fb05e602b7fafd5910b8432c1a5817c
Lewis <Lewis-saunders@live.nmit.ac.nz>
2021-03-16 13:53:05 +1300

    Added work

--- src/weekfour.js
+++ src/weekfour.js
@@ -44,3 +44,45 @@ if(x === 'Ali' && typeof 'string') {
 
 //Double bars "||" = or and double & symbol "&&" = and
 
+x = '1234567'
+console.log(typeof x)
+let y = Number (x)
+// Console.log()
+console.log(typeof y)
+const a = 1+1
+console.log(1+1)
+const b = (a + 2)
+console.log(b)
+const information = {
+    student : 'Lewis',
+    country : 'New Zealand',
+    city : 'Nelson',
+    postCode : 7010
+}
+
+let info = {
+    name : 'Lewis',
+    gender : 'Male',
+    height : '183cm',
+    weight : '111.11',
+    age : '26'
+}
+
+let c = 12
+
+if(c === 12 || typeof c ==='string') {
+console.log('its true')
+}
+else {
+    console.log('error')
+}
+
+let t =5 // declaring a = 5
+let s = t // declaring b = 5
+console.log(t) // true
+console.log(s) // true
+console.log(t===s) // true
+t = 10 // declaring T =10 now
+console.log(t) // value =10 now
+console.log(s) // value =5 still
+console.log(t===s) // stated as false because t no longer is declared as 5, therefore t no longer = S
