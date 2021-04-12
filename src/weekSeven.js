98e0273caffdb5034e1281889dd8ac0b1ec37995
Lewis <Lewis-saunders@live.nmit.ac.nz>
2021-04-12 14:39:56 +1200

    im braindead now

---
+++ src/weekSeven.js
@@ -0,0 +1,91 @@
+// two arguments, return the two arguments in an array
+
+//function newArr(a, b) {
+//   let arrOne = [a, b];
+//    return arrOne
+//}
+
+//console.log(newArr(1, 2))
+
+// let newArr = (a, b) => [a, b];
+// //console.log(typeof newArr)
+// //console.log(newArr(1, 'Ali'))
+
+// // let newArr = (...a) => a // this is called a spread operator
+// // console.log(typeof newArr)
+// // console.log(newArr(1,'Ali', 3, 4, 5, 6, 7, 7))
+
+// let arrTwo = ['Ali', 4, 5, 6, 7, 8, 9, 11 ]
+
+// console.log(arrTwo[6]+", "+arrTwo[7])
+
+//arr = [2, 6]
+
+// function oddOrEven(x) {
+//     let sum = 0;
+//     x.forEach(number => sum += number)
+//     return sum%2 === 0 ? 'even' : 'odd'
+// }
+
+// function oddOrEven(x) {
+//     let sum = 0;
+//     for (let i = 0; i < x.length; i++) {
+//         sum += x[i]
+//         if (sum%2 === 0) {
+//             return 'even'
+//         } else {
+//             return 'odd'
+//         }
+//     }
+// }
+
+// console.log(oddOrEven([1, 2, 3, 4, 5]))
+
+// arr = ["9.4", "4.2"]
+
+// function stringToNumber(arr) {
+//     return arr.map(x => Number(x));
+// }
+
+// function stringToNumber(arr) {
+//     newArr = [] 
+//     arr.forEach((x) => {
+//         newArr.push(parseInt(x))
+//     })
+//     return newArr
+// }
+// console.log(stringToNumber(["9.4", "4.2"]))
+
+// [1, 2, 3, 4, 5, 6] = "1234567"
+
+// function arrayToString(arr) {
+//     let text = " "
+//     for (let i = 0; i < arr.length; i++){
+//         text += arr[i]
+//     }
+//     return text
+// }
+// let example = arrayToString([1, 2, 3, 4, 5, 6])
+// console.log(example)
+// console.log(typeof example)
+
+// function arrayToString(arr) {
+//     return arr.join('');
+// }
+
+// console.log(arrayToString([1, 2, 3, 4, 5, 6]))
+
+// ('hello') = ('olleh')
+
+// function gnirts(x) {
+//     let arr = x.split('')
+//         console.log(arr)
+//     let newArr = arr.reverse()
+//         console.log(newArr)
+//     let joinNewArr = newArr.join ('')
+//         console.log(joinNewArr)
+//     return joinNewArr
+// }
+
+
+console.log(gnirts('Ali'))
